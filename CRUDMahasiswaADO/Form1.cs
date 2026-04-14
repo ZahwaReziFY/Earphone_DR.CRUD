6f5328f452b4021b60fefc20e9e1663b2cb78508
ZahwaReziFY <zahwarezi416@gmail.com>
2026-04-14 10:45:32 +0700

    membuat void Form1 Load

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -277,10 +277,17 @@ namespace CRUDMahasiswaADO
             txtKodeProdi.Clear();
         }
 
-
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+            cmbJK.Items.Clear();
 
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.CellClick += dataGridView1_CellClick;
         }
     }
 }
